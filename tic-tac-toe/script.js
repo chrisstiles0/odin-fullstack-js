2acfba9211d6954f6216d65e7df262a2f1336a69
Chris <cs924@kent.ac.uk>
2026-08-19 12:31:29 +0100

    Add win condition check

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -4,10 +4,12 @@ const cellState = Object.freeze({
     O: "O"
 });
 
-const gameboard = (() => {
-    const width = 3;
-    const height = 3;
+// Board dims
+const width = 3;
+const height = 3;
 
+
+const gameboard = (() => {
     const board = Array.from(
         { length: height },
         () => Array(width).fill(cellState.EMPTY)
@@ -18,14 +20,17 @@ const gameboard = (() => {
             return false;
         }
 
-        board[x][y] = state;
+        board[y][x] = state;
         return true;
     };
+
+    const getCell = (x, y) => board[y]?.[x];
 
     const getBoard = () => board;
 
     return {
         setCell,
+        getCell,
         getBoard
     };
 
@@ -45,13 +50,58 @@ const gameController = (() => {
     const playTurn = (x, y) => {
         const validMove = gameboard.setCell(x, y, currentPlayer);
 
-        if (validMove) {switchPlayer()};
+        if (!validMove) {
+            return false;
+        }
 
-        return validMove;
+        if (checkWin(x, y, currentPlayer)) {
+            console.log("win");
+            return true;
+        }
+
+        switchPlayer();
+        return true;
     };
 
-    const checkWin = () => {
+    const getCurrentPlayer = () => currentPlayer;
 
+    const checkWin = (x, y, player) => {
+        const directions = [
+            [1, 0],   // |
+            [0, 1],   // -
+            [1, 1],   // \
+            [1, -1]   // /
+        ];
+
+        for (const [dx, dy] of directions) {
+            let count = 1;
+
+            // forwards check
+            let currentX = x + dx;
+            let currentY = y + dy;
+
+            while (gameboard.getCell(currentX, currentY) === player) {
+                count++;
+                currentX += dx;
+                currentY += dy;
+            }
+
+            // backwards check
+            currentX = x - dx;
+            currentY = y - dy;
+
+            while (gameboard.getCell(currentX, currentY) === player) {
+                count++;
+                currentX -= dx;
+                currentY -= dy;
+            }
+
+            if (count >= 3) {
+                return true;
+            }
+        }
+
+        return false;
     };
     
     return {
@@ -64,3 +114,12 @@ const gameController = (() => {
 
 
 
+// TEST
+gameController.playTurn(0,0);
+gameController.playTurn(0,0);
+gameController.playTurn(1,0);
+gameController.playTurn(1,1);
+gameController.playTurn(2,1);
+
+
+console.log(gameboard.getBoard());
